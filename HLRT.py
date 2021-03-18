c24d2e5dffdb19f2376686dec9f16506c16c88bd
NguyenTien110 <tienmetien011111@gmail.com>
2021-03-18 12:08:40 +0700

    remove duplicate

--- HLRT.py
+++ HLRT.py
@@ -1,4 +1,5 @@
 import requests
+import textwrap
 
 
 with open('list_link_carmudi.txt') as f:
@@ -7,5 +8,29 @@ with open('list_link_carmudi.txt') as f:
         list_link[i] = list_link[i].replace('\n', '')
     f.close()
 
+# print(list_link)
 
+page1 = requests.get(list_link[0])
+page = [textwrap.dedent(line.rstrip()) for line in page1.content.decode('utf-8').split('\n')]
+print(page)
 # html = html.content.decode('utf-8').splitlines()
+
+HLRT = []  # HLRT is void at the begining
+
+i = 0
+while i < len(page) and page[i] != "<hr>":  # skip first occurrece of head
+    i += 1
+while i < len(page) and page[i] != "</body>":  # l1 is before next tail
+    if "<h3>" in page[i]:  # extract atributes from lk to rk
+        LR = [page[i].replace("<h3>", "").replace("</h3>", "")]  # LR is void at the begining
+        i += 1
+        while i < len(page) and page[i] != "</div>":  # while end of rk
+            if "<li>" in page[i]:
+                li = page[i].split("</b>")
+                LR.append(li[1].replace("</li>", ""))
+            i += 1
+        HLRT.append(LR)  # save tuples
+    else:
+        i += 1
+
+print(HLRT)
