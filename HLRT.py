b7331519e528771589e1ca543162d75f4205c212
NguyenTien110 <tienmetien011111@gmail.com>
2021-03-19 17:27:00 +0700

    hlrt carmudi

--- HLRT.py
+++ HLRT.py
@@ -1,36 +1,42 @@
 import requests
 import textwrap
 
-
-with open('list_link_carmudi.txt') as f:
-    list_link = f.readlines()
-    for i in range(len(list_link)):
-        list_link[i] = list_link[i].replace('\n', '')
-    f.close()
-
-# print(list_link)
-
-page1 = requests.get(list_link[0])
-page = [textwrap.dedent(line.rstrip()) for line in page1.content.decode('utf-8').split('\n')]
-print(page)
-# html = html.content.decode('utf-8').splitlines()
-
+link = 'https://www.carmudi.vn/mua-ban-o-to/'
 HLRT = []  # HLRT is void at the begining
 
-i = 0
-while i < len(page) and page[i] != "<hr>":  # skip first occurrece of head
-    i += 1
-while i < len(page) and page[i] != "</body>":  # l1 is before next tail
-    if "<h3>" in page[i]:  # extract atributes from lk to rk
-        LR = [page[i].replace("<h3>", "").replace("</h3>", "")]  # LR is void at the begining
-        i += 1
-        while i < len(page) and page[i] != "</div>":  # while end of rk
-            if "<li>" in page[i]:
-                li = page[i].split("</b>")
-                LR.append(li[1].replace("</li>", ""))
-            i += 1
-        HLRT.append(LR)  # save tuples
-    else:
+
+def get_html(link_get):
+    html = requests.get(link_get)
+    lines = [textwrap.dedent(line.rstrip()) for line in html.content.decode('utf-8').split('\n')]
+    return lines
+
+
+def main():
+    i = 0
+    html = get_html(link)
+    while i < len(html) and not html[i].endswith('<section id="listings" class="list ">'):  # HEAD LEFT
         i += 1
 
-print(HLRT)
+    while i < len(html) and html[i] != '<button class="btn-advisory hide">Liên hệ tư vấn</button>':  # RIGHT TAIL
+        if html[i].startswith('<article class="media ">'):  # extract atributes from lk to rk
+            data = {}
+            i += 1
+            count = 0
+            small_element = ['type', 'location', 'transmission', 'build']
+            while i < len(html) and html[i] != '<hr class="dotted-line">':
+                if '<h3 class="list-info-title">' == html[i]:
+                    data['title'] = html[i + 1]
+                elif '<small' in html[i]:
+                    data[small_element[count]] = html[i].replace('<small>', '').replace('</small>', '')
+                    count += 1
+                elif 'class="list-price-number"' in html[i]:
+                    data['price'] = html[i + 1]
+                i += 1
+            HLRT.append(data)  # save data
+        else:
+            i += 1
+    print(HLRT)
+
+
+if __name__ == '__main__':
+    main()
